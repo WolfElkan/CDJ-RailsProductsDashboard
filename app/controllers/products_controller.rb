c2afa4279964618dfa1a466b03d37c63edb199b4
Wolf Elkan <wolf.elkan@gmail.com>
2017-02-01 21:36:39 -0500

    Part 2: mostly idiot-proof

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,45 +1,58 @@
 class ProductsController < ApplicationController
 
-  def index
-    @products = Product.all
-  end
+	def index
+		@products = Product.all
+	end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+	def show
+		@product = Product.find(params[:id])
+	end
 
-  def new
-    @categories = Category.all
-  end
+	def new
+		session.init categories: Category.all, product: Product.new, error: ""
+		@categories = session[:categories]
+		@product    = session[:product]
+		@error      = session[:error]
+		# render plain: session[:categories][3]['id']
+	end
 
-  def create
-    # puts product_params
-    Product.create(product_params)
-    redirect_to '/products'
-  end
+	def create
+		# If this were a larger application, I'd put this logic in the model
+		# as validation, but I don't know how to render errors to the page.
+		# Perhaps I'll do it on a supsequent commit.
+		if Category.where(id:product_params[:category_id]).any?
+			Product.create(product_params)
+			session.clear
+			redirect_to '/products'
+		else
+			session[:product] = product_params
+			session[:error] = "Please select a Category"
+			redirect_to '/products/new'
+		end
+	end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+	def edit
+		@product = Product.find(params[:id])
+	end
 
-  def update
-    Product.find(params[:id]).update(product_params)
-    redirect_to '/products'
-  end
+	def update
+		Product.find(params[:id]).update(product_params)
+		redirect_to '/products'
+	end
 
-  def destroy
-    Product.delete(params[:id])
-    redirect_to '/products'
-  end
+	def destroy
+		Product.delete(params[:id])
+		redirect_to '/products'
+	end
 
 end
 
-  private
-  def product_params
-    {
-      name:        params[:name],
-      description: params[:description],
-      price:       params[:price],
-      category_id: params[:category_id]
-    }
-  end
+	private
+	def product_params
+		{
+			name:        params[:name],
+			description: params[:description],
+			price:       params[:price],
+			category_id: params[:category_id]
+		}
+	end
